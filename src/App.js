f0dd9b334d63fcd63f01fe0cccadc2a8d2a9b578
Fahim1272 <imfahim72@gmail.com>
2023-07-14 06:42:40 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,56 +1,103 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import { useEffect } from 'react';
 import './App.css';
+import Protected from './features/auth/components/Protected';
+import CartPage from './pages/CartPage';
+import Checkout from './pages/Checkout';
+import Home from './pages/Home';
+import Loginpage from './pages/Loginpage';
+import ProductDetailsPage from './pages/ProductDetailsPage';
+import SignupPage from './pages/SignupPage';
+import PageNotFound from './pages/404';
+import OrderSuccessPage from './pages/OrderSuccessPage';
+import UserOrdersPage from './pages/UserOrdersPage';
+import {createBrowserRouter,RouterProvider} from "react-router-dom";
+import { fetchItemsByUserIdAsync } from './features/cart/cartSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectLoggedInUser } from './features/auth/authSlice';
+import UserProfilePage from './pages/UserProfilePage';
+import Logout from './features/auth/components/Logout';
+import ForgotPasswordPage from './pages/ForgotPasswordPage';
+import { fetchLoggedInUserAsync } from './features/user/userSlice';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<Protected><Home></Home></Protected>,
+  },
+  {
+    path: "/login",
+    element: <Loginpage></Loginpage>,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage></SignupPage>,
+  },
+  {
+    path: "/cart",
+    element: <Protected><CartPage></CartPage> </Protected>,
+  },
+  {
+    path: "/checkout",
+    element: <Protected><Checkout></Checkout></Protected>,
+  },
+  {
+    path: "/product-details/:id",
+    element: <Protected><ProductDetailsPage></ProductDetailsPage></Protected>,
+  },
+  {
+    path: '/order-success/:id',
+    element: (
+      <OrderSuccessPage></OrderSuccessPage>
+    ),
+  },
+  {
+    path: '/orders',
+    element: (
+      <UserOrdersPage></UserOrdersPage>
+      // we will add Page later right now using component directly.
+    ),
+  },
+  {
+    path: '/profile',
+    element: (
+      <UserProfilePage></UserProfilePage>
+      // we will add Page later right now using component directly.
+    ),
+    
+  },
+  {
+    path: '/logout',
+    element: <Logout></Logout>,
+  },
+  {
+    path: '/forgot-password',
+    element: <ForgotPasswordPage></ForgotPasswordPage>,
+  },
+  {
+    path: '*',
+    element: (
+      <PageNotFound></PageNotFound>
+    ),
+  },
+]);
+
+
+
 
 function App() {
+  const dispatch = useDispatch();
+  const user = useSelector(selectLoggedInUser)
+
+  useEffect(() => {
+    if (user) {
+      dispatch(fetchItemsByUserIdAsync(user.id));
+      dispatch(fetchLoggedInUserAsync(user.id));
+    }
+
+  }, [dispatch, user])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <RouterProvider router={router} />
     </div>
   );
 }
